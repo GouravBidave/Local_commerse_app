e662b7068049c1d57567440f029cb63319f9c38f
garry <gouravsbidave7898@gmail.com>
2026-02-09 14:16:31 +0530

    Dynamic product rendering in template

--- core/store/views.py
+++ core/store/views.py
@@ -6,5 +6,22 @@ from django.http import HttpResponse
 # def home(request):
 #     return HttpResponse("Welcome to my local commerce app 🚀")
 
+# def home(request):
+#     return render(request,'home.html')
+from django.shortcuts import render
+
 def home(request):
-    return render(request,'home.html')
+
+    products = [
+        {"name": "Rice", "price": 50},
+        {"name": "Milk", "price": 30},
+        {"name": "Bread", "price": 25},
+    ]
+
+    context = {
+        "products": products
+    }
+
+    return render(request, "home.html", context)
+
+
